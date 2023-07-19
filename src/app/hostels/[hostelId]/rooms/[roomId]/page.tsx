7b5f77ed75dbed726ecabcdcfdb540abd6b26a2e
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-07-19 21:05:07 +0000

    Added a NameStatsBar section

--- src/app/hostels/[hostelId]/rooms/[roomId]/page.tsx
+++ src/app/hostels/[hostelId]/rooms/[roomId]/page.tsx
@@ -1,6 +1,13 @@
+// ! Inspiration Airbnb: https://www.airbnb.com/rooms/9386477?adults=1&category_tag=Tag%3A8225&children=0&enable_m3_private_room=true&infants=0&pets=0&search_mode=flex_destinations_search&check_in=2023-12-16&check_out=2023-12-21&source_impression_id=p3_1689792503_L6GnMWuS4dWkIUh0&previous_page_section_name=1000&federated_search_id=8353e899-6073-422f-b883-c596a38b67c2
+import NameStatsBar from "@/components/hostels/hostelId/rooms/roomId/NameStatsBar";
 import React from "react";
 
 export default function RoomId({ params }: { params: { roomId: string } }) {
-  // Use useSearchParams to get the hostel ID from the URL
-  return <div>Hostel Room: {params.roomId} </div>;
+  // Use useSearchParams or useRouter(preferred) to to get the hostel ID from the URL
+  return (
+    <main className="horizontal-padding">
+      {/* Top bar with name, starts, number of review, location, share and save buttons */}
+      <NameStatsBar />
+    </main>
+  );
 }
---
+++ src/components/hostels/hostelId/rooms/PicturesSection.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PictureSection() {
+  return <div>Picture Section</div>;
+}
