c060e2490b0c27f9fc1acdecbabead1a1ad4d88c
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-08-20 10:50:24 +0000

    Made a change to the Featured Section place

---
+++ src/components/hostels/hostelId/rooms/roomId/AboutRoom.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function AboutRoom() {
+  return (
+    <>
+      <h2>About this room</h2>
+      <p>
+        Snow igloo with real beds inside. Nice atmosphere with led lights and
+        white walls. Outside the igloo is possibility to see northern lights if
+        you are lucky. Come to Lapland and explore the wilderness. It&apos;s
+        always minus degrees&apos; inside the igloo. We provide warm sleeping
+        bag but you should bring your thermos layers, warm hat, socks and
+        cloves.
+      </p>
+    </>
+  );
+}
